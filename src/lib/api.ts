2fcd4b04ae74359ae41e8916ef51d5b128625cb8
pndroa <157326278+pndroa@users.noreply.github.com>
2025-04-15 21:10:20 +0200

    fixed axios instance and env template

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 export const apiInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    Accept: 'application/json',
   },
 })
