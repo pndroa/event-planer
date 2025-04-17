0cb92019925479b48555a7cfa73142f1d8bc10c5
pndroa <157326278+pndroa@users.noreply.github.com>
2025-04-17 21:14:51 +0200

    rename apiInstance to api for consistency

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const apiInstance = axios.create({
+export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
