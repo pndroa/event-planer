3848461e264aa7f00c2b438a747e20c3128e7f65
pndroa <157326278+pndroa@users.noreply.github.com>
2025-04-29 23:50:16 +0200

    fix: removed test api for deployment

--- src/app/api/test/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import prisma from '@/lib/client'
-
-export const dynamic = 'force-dynamic' // oder 'auto'
-
-export async function GET() {
-  const user = await prisma.auth_users.findMany()
-  return NextResponse.json({ user, message: 'test api' }, { status: 200 })
-}
