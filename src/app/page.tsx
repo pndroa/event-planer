cb3f9cbc608888f9ab24d4faa7d13ffb6cbec49f
pndroa <157326278+pndroa@users.noreply.github.com>
2025-04-29 18:23:05 +0200

    refactor: replace Homepage component with welcome message in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 'use client'
-import { Box } from '@mui/material'
-import Homepage from '@/components/home'
+import { Box, Typography } from '@mui/material'
 
 export default function Home() {
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: 'auto' }}>
-      <Homepage />
+      <Typography variant='h1'>Welcome to the Event-planner</Typography>
     </Box>
   )
 }
