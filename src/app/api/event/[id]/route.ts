385cc5e6cfe8ad27afae30a6776d8bce3ad1dd98
pndroa <157326278+pndroa@users.noreply.github.com>
2025-05-07 15:20:12 +0200

    feat: implement GET API for retrieving event by ID

---
+++ src/app/api/event/[id]/route.ts
@@ -0,0 +1,26 @@
+import { getServerAuth } from '@/lib/auth'
+import prisma from '@/lib/client'
+import { NextResponse } from 'next/server'
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  const { errorResponse } = await getServerAuth()
+
+  if (errorResponse) return errorResponse
+
+  const { id } = params
+
+  if (!id) {
+    throw new Error('Invalid or missing id parameter')
+  }
+
+  try {
+    const event = await prisma.events.findUnique({
+      where: {
+        eventId: id,
+      },
+    })
+    return NextResponse.json({ event }, { status: 200 })
+  } catch (error) {
+    return NextResponse.json({ error })
+  }
+}
