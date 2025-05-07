f73311942e8b8d4303cca4104d3568898286c5ce
pndroa <157326278+pndroa@users.noreply.github.com>
2025-05-07 13:15:39 +0200

    fix: add server authentication to GET and POST survey questions

--- src/app/api/survey/surveyQuestion/route.ts
+++ src/app/api/survey/surveyQuestion/route.ts
@@ -1,7 +1,12 @@
+import { getServerAuth } from '@/lib/auth'
 import prisma from '@/lib/client'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
+  const { errorResponse } = await getServerAuth()
+
+  if (errorResponse) return errorResponse
+
   try {
     const surveyQuestions = await prisma.surveyQuestions.findMany()
     return NextResponse.json({ surveyQuestions }, { status: 200 })
@@ -11,6 +16,10 @@ export async function GET() {
 }
 
 export async function POST(request: Request) {
+  const { errorResponse } = await getServerAuth()
+
+  if (errorResponse) return errorResponse
+
   try {
     const body = await request.json()
 
